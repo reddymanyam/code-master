f4401eddcf9cc195e8a7afb0201408d3619411f1
reddymanyam <reddappareddy18@gmail.com>
2024-05-23 13:03:02 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,52 @@
 import './App.css';
 import Explore from './components/Explore';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Problems from './components/Problems';
 import Contest from './components/Contest';
 import LeaderBoard from './components/LeaderBoard';
 import Register from './components/Register';
 import Login from './components/Login';
+import Home from './components/Home';
+import CodeEditor from './components/CodeEditor';
+import Profile from './components/Profile';
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
   const contest = {
     name: 'Global Code Challenge',
     description: 'Compete with top coders from around the world.',
-    date: '2024-06-25T14:00:00', 
-    link: 'https://www.example.com/register'
+    date: '2024-06-25T14:00:00',
+    link: 'https://www.example.com/register',
+  };
+
+  const login = (user) => {
+    setUser(user);
+    navigate('/profile');
+  };
+
+  const logout = () => {
+    setUser(null);
+    navigate('/login');
   };
 
   return (
-    
     <div>
-    <Navbar />   
-        <Routes>
-         <Route  path='/explore' element={<Explore />} />
-         <Route  path='/problems' element={<Problems problem />} />
-         <Route path='/contest' element={<Contest contest={contest}/>} />
-         <Route path='/leaderboard' element={<LeaderBoard />} />
-         <Route path='/register' element={<Register />} />
-         <Route path='/login' element={<Login />} />
-        </Routes>
-      
+      <Navbar user={user} onLogout={logout} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/problems" element={<Problems problem />} />
+        <Route path="/contest" element={<Contest contest={contest} />} />
+        <Route path="/leaderboard" element={<LeaderBoard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login onLogin={login} />} />
+        <Route path="/profile" element={user ? <Profile user={user} /> : <Login onLogin={login} />} />
+        <Route path="/codeeditor" element={<CodeEditor />} />
+      </Routes>
     </div>
   );
 }
