206748c309fe320dcdf570914205710b858ce79d
reddymanyam <reddappareddy18@gmail.com>
2024-05-23 13:05:55 +0530

    register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,24 +1,28 @@
 import React, { useState } from 'react';
-import { Box, TextField, Paper, Avatar, Typography, Button } from '@mui/material';
+import { Box, TextField, Paper, Avatar, Typography, Button, Alert } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
   const [value, setValue] = useState({
-    fname:"",
-    sname:"",
-    number:"",
-    password:""
+    fname: "",
+    sname: "",
+    number: "",
+    password: ""
   });
-  
-  const handleSubmit = () =>{
-    const{fname,sname,number,password} = value;
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
 
-    if(!fname || !sname || !number || !password) {
-      alert("please enter the details")
-    }
-    else{
-      alert("Succesfully Register")
+  const handleSubmit = () => {
+    const { fname, sname, number, password } = value;
+
+    if (!fname || !sname || !number || !password) {
+      setError("Please enter all the details");
+    } else {
+      alert("Successfully Registered");
+      navigate('/login');
     }
   }
+
   return (
     <Box 
       sx={{
@@ -54,19 +58,20 @@ const Register = () => {
           <Typography variant='h5' component="h4">Register</Typography> 
           <Avatar src="/broken-image.jpg" />
         </Box>
+        {error && <Alert severity="error">{error}</Alert>}
         <TextField
           required
           type='text'
           label="First Name"
           value={value.fname}
-          onChange={e => setValue({...value, fname:e.target.value})}
+          onChange={e => setValue({...value, fname: e.target.value})}
         />
         <TextField
           required
           type='text'
           label="Second Name"
           value={value.sname}
-          onChange={e =>setValue({...value, sname:e.target.value})}
+          onChange={e => setValue({...value, sname: e.target.value})}
         />
         <TextField
           required
@@ -76,7 +81,7 @@ const Register = () => {
             shrink: true,
           }}
           value={value.number}
-          onChange={e =>setValue({...value, number:e.target.value})}
+          onChange={e => setValue({...value, number: e.target.value})}
         />
         <TextField
           required
@@ -84,7 +89,7 @@ const Register = () => {
           type="password"
           autoComplete="current-password"
           value={value.password}
-          onChange={e => setValue({...value, password:e.target.value})}
+          onChange={e => setValue({...value, password: e.target.value})}
         />
         <Button variant='contained' onClick={handleSubmit}>Register</Button>
       </Paper>
